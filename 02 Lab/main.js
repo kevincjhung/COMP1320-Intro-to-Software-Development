d0bfa1c2539802eaabb6e83217cf65eb17b393b6
kevincjhung <73084624+kevincjhung@users.noreply.github.com>
2021-10-02 11:11:52 -0700

    Update main.js
    formatting

--- 02 Lab/main.js
+++ 02 Lab/main.js
@@ -30,25 +30,25 @@ getDayOfTheWeek(userYear, userMonth, userDay);
 
 
 function makeCalendar() { 
-	/*  print out the date and the day of the week using loops for each day in 2021
-	    start with jan 1st on friday */
-	   
-	let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	let nameOfDay = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-	const monthsInAYear = 12;
+    /*  print out the date and the day of the week using loops for each day in 2021 start with jan 1st on friday */ 
+    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let nameOfDay = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const monthsInAYear = 12;
     let dayOfWeek = 5; // tracks day of week, 4 because jan 1st 2021 is a Thursday
 	
-	for(let month = 1; month <= monthsInAYear; month++){
-		for(let day = 1; day <= daysInMonth[month-1]; day++){
-            	console.log(month + "-" + day + "-2021 " + " is a " + nameOfDay[dayOfWeek]);
+    for(let month = 1; month <= monthsInAYear; month++){	
+        for(let day = 1; day <= daysInMonth[month-1]; day++){
             
-            // increment dayOfWeek variable 
-			if(dayOfWeek == 6) { //wraps around nameOfDay[] if dayOfWeek at end of nameOfDay[] 
-				dayOfWeek = 0; 
-			} else {
-				dayOfWeek++;
-			}
-		}
+	    console.log(month + "-" + day + "-2021 " + " is a " + nameOfDay[dayOfWeek]);
+             
+	    // increment dayOfWeek variable 
+	    if(dayOfWeek == 6) { //wraps around nameOfDay[] if dayOfWeek at end of nameOfDay[] 
+	        dayOfWeek = 0; 
+	    } else {
+	        dayOfWeek++;
+	    }
 	}
+    }
 }
+
 makeCalendar();
