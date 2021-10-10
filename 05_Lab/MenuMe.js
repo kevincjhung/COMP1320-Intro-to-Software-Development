6b71df738369685f706bb43853c1cee9f97367f4
kevincjhung <73084624+kevincjhung@users.noreply.github.com>
2021-10-09 19:57:48 -0700

    Update MenuMe.js
    Read csv file into 2 dimensional array

--- 05_Lab/MenuMe.js
+++ 05_Lab/MenuMe.js
@@ -1,1 +1,21 @@
+const parse = require('csv-parse');
+const fs = require('fs');
 
+let csvData = []; // array used to store data from csv file
+
+fs.createReadStream(__dirname + '/menu1.csv',) //current directory + file name
+    .pipe(
+        parse({
+            delimiter: ',' //values separated by comma
+        })
+    )
+    .on('data', function (dataRow){
+        csvData.push(dataRow);
+    })
+    .on('end', function (){   
+        console.log(csvData);
+});
+
+/**
+* The code at this point will read the csv file from menu1.csv and store it in the variable csvData[]
+*/
